6c026484fb818eb55d945e359db03a99e14fe341
Constantinos Venetsanopoulos <cven@grnet.gr>
2013-08-02 13:44:00 +0300

    Annotate snf-cyclades-app settings
    Make sure for every setting that gets annotated to change the
    corresponding 'getattr' entry to a 'settings.SETTING' entry.
    
    Conflicts:
    	snf-cyclades-app/synnefo/app_settings/default/api.py
    	snf-cyclades-app/synnefo/app_settings/default/backend.py
    	snf-cyclades-app/synnefo/app_settings/default/plankton.py
    	snf-cyclades-app/synnefo/app_settings/default/ui.py
    	snf-cyclades-app/synnefo/app_settings/default/userdata.py
    	snf-cyclades-app/synnefo/ui/views.py

--- snf-cyclades-app/synnefo/app_settings/default/plankton.py
+++ snf-cyclades-app/synnefo/app_settings/default/plankton.py
@@ -1,20 +1,36 @@
-# -*- coding: utf-8 -*-
-#
+from synnefo.settings.setup import Setting, Mandatory, Default
+
 # Plankton configuration
 ########################
 
-# Backend settings
-BACKEND_DB_CONNECTION = 'sqlite:////usr/share/synnefo/pithos/backend.db'
+BACKEND_DB_CONNECTION = Mandatory(
+    example_value="sqlite:////usr/share/synnefo/pithos/backend.db or "
+        "postgresql://user:password@host:port/db_name",
+    description="URI pointing to the Pithos database.",
+    category="snf-cyclades-app-plankton",
+)
+
 PITHOS_BACKEND_POOL_SIZE = 8
 
-# The Pithos container where images will be stored by default
-DEFAULT_PLANKTON_CONTAINER = 'images'
+ALLOWED_DISK_FORMATS = Default(
+    default_value=("diskdump", "extdump", "ntfsdump"),
+    description="The allowed image formats. These are the only formats "
+        "supported by Synnefo.",
+    export=False,
+)
 
-ALLOWED_DISK_FORMATS = ('diskdump', 'extdump', 'ntfsdump')
-DEFAULT_DISK_FORMAT = 'diskdump'
+DEFAULT_DISK_FORMAT = Default(
+    default_value="diskdump",
+    example_value="diskdump",
+    description="The default image format used by Synnefo.",
+    export=False,
+)
 
-ALLOWED_CONTAINER_FORMATS = ('aki', 'ari', 'ami', 'bare', 'ovf')
-DEFAULT_CONTAINER_FORMAT = 'bare'
+ALLOWED_CONTAINER_FORMATS = Default(
+    default_value=("aki", "ari", "ami", "bare", "ovf"),
+    description="The allowed image container formats.",
+    export=False,
+)
 
 # The owner of the images that will be marked as "system images" by the UI
 SYSTEM_IMAGES_OWNER = 'okeanos'
@@ -27,3 +43,9 @@ PITHOS_BACKEND_XSEG_POOL_SIZE = 8
 
 # The maximum interval (in seconds) for consequent backend object map checks
 PITHOS_BACKEND_MAP_CHECK_INTERVAL = 1
+
+DEFAULT_CONTAINER_FORMAT = Default(
+    default_value="bare",
+    description="The default image container format.",
+    export=False,
+)
