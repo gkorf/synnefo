d5babb4848f7a18fcaac8eea657a05416334c7fc
Constantinos Venetsanopoulos <cven@grnet.gr>
2013-08-05 10:59:56 +0300

    Annotate snf-pithos-app settings
    Create the 'settings/default/' directory and move all settings there.
    Update 'setup.py' accordingly.
    
    Conflicts:
    	snf-pithos-app/pithos/api/settings.py

--- snf-pithos-app/pithos/api/settings.py
+++ snf-pithos-app/pithos/api/settings.py
@@ -47,8 +47,6 @@ VIEW_PREFIX = get_service_prefix(synnefo_services, 'pithos_view')
 ASTAKOS_ACCOUNTS_PREFIX = get_service_prefix(synnefo_services,
                                              'astakos_account')
 ASTAKOS_VIEWS_PREFIX = get_service_prefix(synnefo_services, 'astakos_ui')
-ASTAKOS_KEYSTONE_PREFIX = get_service_prefix(synnefo_services,
-                                             'astakos_identity')
 
 
 # --------------------------------------------------------------------
@@ -107,78 +105,63 @@ ASTAKOS_UI_PROXY_PATH = join_urls(BASE_PATH, ASTAKOS_UI_PREFIX)
 # Astakos login URL to redirect if the user information is missing
 LOGIN_URL = join_urls(ASTAKOS_UI_PROXY_PATH, 'login')
 
+ASTAKOS_KEYSTONE_PREFIX = get_service_prefix(synnefo_services, 'astakos_identity')
+
+BASE_ASTAKOS_PROXY_PATH = getattr(settings, 'PITHOS_BASE_ASTAKOS_PROXY_PATH',
+                                  ASTAKOS_BASE_PATH)
+BASE_ASTAKOS_PROXY_PATH = join_urls(BASE_PATH, BASE_ASTAKOS_PROXY_PATH)
+BASE_ASTAKOS_PROXY_PATH = BASE_ASTAKOS_PROXY_PATH.strip('/')
+
+
+SERVICE_TOKEN = settings.PITHOS_SERVICE_TOKEN
+
+COOKIE_NAME = settings.PITHOS_ASTAKOS_COOKIE_NAME
+
+ASTAKOSCLIENT_POOLSIZE = settings.PITHOS_ASTAKOSCLIENT_POOLSIZE
+BACKEND_POOL_ENABLED = settings.PITHOS_BACKEND_POOL_ENABLED
+BACKEND_POOL_SIZE = settings.PITHOS_BACKEND_POOL_SIZE
+
+BACKEND_DB_MODULE = settings.PITHOS_BACKEND_DB_MODULE
+BACKEND_BLOCK_MODULE = settings.PITHOS_BACKEND_BLOCK_MODULE
+
+BACKEND_DB_CONNECTION = settings.PITHOS_BACKEND_DB_CONNECTION
+BACKEND_BLOCK_PATH = settings.PITHOS_BACKEND_BLOCK_PATH
+BACKEND_BLOCK_SIZE = settings.PITHOS_BACKEND_BLOCK_SIZE
+BACKEND_HASH_ALGORITHM = settings.PITHOS_BACKEND_HASH_ALGORITHM
+
+UPDATE_MD5 = settings.PITHOS_UPDATE_MD5
+
+BACKEND_VERSIONING = settings.PITHOS_BACKEND_VERSIONING
+BACKEND_FREE_VERSIONING = settings.PITHOS_BACKEND_FREE_VERSIONING
+
+RADOS_STORAGE = settings.PITHOS_RADOS_STORAGE
+RADOS_POOL_BLOCKS = settings.PITHOS_RADOS_POOL_BLOCKS
+RADOS_POOL_MAPS = settings.PITHOS_RADOS_POOL_MAPS
+
+PROXY_USER_SERVICES = settings.PITHOS_PROXY_USER_SERVICES
+
+PUBLIC_URL_SECURITY = settings.PITHOS_PUBLIC_URL_SECURITY
+PUBLIC_URL_ALPHABET = settings.PITHOS_PUBLIC_URL_ALPHABET
+
+API_LIST_LIMIT = settings.PITHOS_API_LIST_LIMIT
+
+#
+# Obsolete settings, these should go away along with the relevant code.
+#
+
+BACKEND_BLOCK_UMASK = settings.PITHOS_BACKEND_BLOCK_UMASK
+
+BACKEND_QUEUE_MODULE = settings.PITHOS_BACKEND_QUEUE_MODULE
+BACKEND_QUEUE_HOSTS = settings.PITHOS_BACKEND_QUEUE_HOSTS
+BACKEND_QUEUE_EXCHANGE = settings.PITHOS_BACKEND_QUEUE_EXCHANGE
+
+BACKEND_ACCOUNT_QUOTA = settings.PITHOS_BACKEND_ACCOUNT_QUOTA
+BACKEND_CONTAINER_QUOTA = settings.PITHOS_BACKEND_CONTAINER_QUOTA
+
+TRANSLATE_UUIDS = settings.PITHOS_TRANSLATE_UUIDS
 
 # --------------------------------------------------------------------
 # Backend settings
-
-# SQLAlchemy (choose SQLite/MySQL/PostgreSQL).
-BACKEND_DB_MODULE = getattr(
-    settings, 'PITHOS_BACKEND_DB_MODULE', 'pithos.backends.lib.sqlalchemy')
-BACKEND_DB_CONNECTION = getattr(settings, 'PITHOS_BACKEND_DB_CONNECTION',
-                                'sqlite:////tmp/pithos-backend.db')
-
-# Block storage.
-BACKEND_BLOCK_MODULE = getattr(
-    settings, 'PITHOS_BACKEND_BLOCK_MODULE', 'pithos.backends.lib.hashfiler')
-BACKEND_BLOCK_PATH = getattr(
-    settings, 'PITHOS_BACKEND_BLOCK_PATH', '/tmp/pithos-data/')
-BACKEND_BLOCK_UMASK = getattr(settings, 'PITHOS_BACKEND_BLOCK_UMASK', 0o022)
-
-# Queue for billing.
-BACKEND_QUEUE_MODULE = getattr(settings, 'PITHOS_BACKEND_QUEUE_MODULE', None)
-# Example: 'pithos.backends.lib.rabbitmq'
-
-BACKEND_QUEUE_HOSTS = getattr(settings, 'PITHOS_BACKEND_QUEUE_HOSTS', None)
-# Example: "['amqp://guest:guest@localhost:5672']"
-
-BACKEND_QUEUE_EXCHANGE = getattr(settings, 'PITHOS_BACKEND_QUEUE_EXCHANGE',
-                                 'pithos')
-
-# Default setting for new accounts.
-BACKEND_ACCOUNT_QUOTA = getattr(
-    settings, 'PITHOS_BACKEND_ACCOUNT_QUOTA', 50 * 1024 * 1024 * 1024)
-BACKEND_CONTAINER_QUOTA = getattr(
-    settings, 'PITHOS_BACKEND_CONTAINER_QUOTA', 0)
-BACKEND_VERSIONING = getattr(settings, 'PITHOS_BACKEND_VERSIONING', 'auto')
-BACKEND_FREE_VERSIONING = getattr(settings, 'PITHOS_BACKEND_FREE_VERSIONING',
-                                  True)
-
-# Enable backend pooling
-BACKEND_POOL_ENABLED = getattr(settings, 'PITHOS_BACKEND_POOL_ENABLED', True)
-
-# Default backend pool size
-BACKEND_POOL_SIZE = getattr(settings, 'PITHOS_BACKEND_POOL_SIZE', 5)
-
-# Update object checksums.
-UPDATE_MD5 = getattr(settings, 'PITHOS_UPDATE_MD5', False)
-
-RADOS_STORAGE = getattr(settings, 'PITHOS_RADOS_STORAGE', False)
-RADOS_POOL_BLOCKS = getattr(settings, 'PITHOS_RADOS_POOL_BLOCKS', 'blocks')
-RADOS_POOL_MAPS = getattr(settings, 'PITHOS_RADOS_POOL_MAPS', 'maps')
-
-# This enables a ui compatibility layer for the introduction of UUIDs in
-# identity management.  WARNING: Setting to True will break your installation.
-TRANSLATE_UUIDS = getattr(settings, 'PITHOS_TRANSLATE_UUIDS', False)
-
-# Set how many random bytes to use for constructing the URL
-# of Pithos public files
-PUBLIC_URL_SECURITY = getattr(settings, 'PITHOS_PUBLIC_URL_SECURITY', 16)
-# Set the alphabet to use for constructing the URL of Pithos public files
-PUBLIC_URL_ALPHABET = getattr(
-    settings,
-    'PITHOS_PUBLIC_URL_ALPHABET',
-    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-
-# The maximum number or items returned by the listing api methods
-API_LIST_LIMIT = getattr(settings, 'PITHOS_API_LIST_LIMIT', 10000)
-
-# The backend block size
-BACKEND_BLOCK_SIZE = getattr(
-    settings, 'PITHOS_BACKEND_BLOCK_SIZE', 4 * 1024 * 1024)
-
-# The backend block hash algorithm
-BACKEND_HASH_ALGORITHM = getattr(
-    settings, 'PITHOS_BACKEND_HASH_ALGORITHM', 'sha256')
 
 # Set the credentials (client identifier, client secret) issued for
 # authenticating the views with astakos during the resource access token
