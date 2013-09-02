fe92812c0de8e95b692f747299e0a76e8e8fd1e7
Georgios D. Tsoukalas <gtsouk@grnet.gr>
2013-09-02 14:04:01 +0300

    stats: fix settings

--- snf-stats-app/synnefo_stats/stats_settings.py
+++ snf-stats-app/synnefo_stats/stats_settings.py
@@ -1,6 +1,17 @@
 from django.conf import settings
-from synnefo.lib import parse_base_url
 
-BASE_URL = getattr(settings, 'STATS_BASE_URL',
-                   'https://stats.example.synnefo.org/stats/')
-BASE_HOST, BASE_PATH = parse_base_url(BASE_URL)
+BASE_URL = settings.STATS_BASE_URL
+BASE_HOST = settings.STATS_BASE_HOST
+BASE_PATH = settings.STATS_BASE_PATH
+
+IMAGE_WIDTH = settings.STATS_IMAGE_WIDTH
+WIDTH = settings.STATS_WIDTH
+HEIGHT = settings.STATS_HEIGHT
+
+RRD_PREFIX = settings.STATS_RRD_PREFIX
+GRAPH_PREFIX = settings.STATS_GRAPH_PREFIX
+
+FONT = settings.STATS_FONT
+
+BAR_BORDER_COLOR = settings.STATS_BAR_BORDER_COLOR
+BAR_BG_COLOR = settings.STATS_BAR_BG_COLOR
