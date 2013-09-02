bb4156d7cc23b0acc3c01bafd922eb0932f3230d
Georgios D. Tsoukalas <gtsouk@grnet.gr>
2013-09-02 13:50:19 +0300

    branding: fix settings
    Conflicts:
    	snf-astakos-app/astakos/im/views/im.py
    	snf-branding/synnefo_branding/utils.py

--- snf-branding/synnefo_branding/settings/default/settings.py
+++ snf-branding/synnefo_branding/settings/default/settings.py
@@ -1,4 +1,4 @@
-from synnefo.settings.setup import Setting, Default
+from synnefo.settings.setup import Setting, Default, Auto
 import datetime
 
 # Branding configuration
@@ -11,8 +11,9 @@ import datetime
 BRANDING_SERVICE_NAME = Default(
     default_value="Synnefo",
     example_value="My Cloud Service",
-    description="Brand name of the cloud service. This will appear on the "
-        "Synnefo UI and emails.",
+    description=(
+        "Brand name of the cloud service. This will appear on the "
+        "Synnefo UI and emails."),
     category="snf-branding-settings",
     export=True,
 )
@@ -20,9 +21,10 @@ BRANDING_SERVICE_NAME = Default(
 BRANDING_COMPANY_NAME = Default(
     default_value="GRNET",
     example_value="My Company name",
-    description="The name of the company behind the service. This will appear "
+    description=(
+        "The name of the company behind the service. This will appear "
         "on the UI's footer message, if enabled, and will be included in the "
-        "emails sent by the service.",
+        "emails sent by the service."),
     category="snf-branding-settings",
     export=True,
 )
@@ -30,8 +32,9 @@ BRANDING_COMPANY_NAME = Default(
 BRANDING_COMPANY_URL = Default(
     default_value="http://www.grnet.gr/en/",
     example_value="http://my.company.com",
-    description="The company's official site URL. This is used on the UI "
-        "footer's Copyright message, if enabled.",
+    description=(
+        "The company's official site URL. This is used on the UI "
+        "footer's Copyright message, if enabled."),
     category="snf-branding-settings",
     export=True,
 )
@@ -40,58 +43,84 @@ BRANDING_COMPANY_URL = Default(
 # Image configuration
 #
 
+
+def _auto_configure_branding_image_media_url(setting, value, deps):
+    if value is Setting.NoValue:
+        # acknowledge user-provided setting
+        return Setting.NoValue
+    # User did not provide setting, create one out of MEDIA_URL
+    from django.conf import settings
+    return settings.MEDIA_URL + "branding/images/",
+
+
+def auto_configure_append_default_value(setting, value, deps):
+    if value is Setting.NoValue:
+        # acknowledge user-provided setting
+        return Setting.NoValue
+    # User did not provide setting, create one by appending
+    return deps[setting.setting_name] + setting.default_value
+
+
 BRANDING_IMAGE_MEDIA_URL = Default(
-    default_value=MEDIA_URL + "branding/images/",
-    description="The default path to the folder that contains all branding "
-        "images. ",
-    dependencies=[MEDIA_URL],
+    default_value=None,
+    description=(
+        "The default path to the folder that contains all branding images. "),
     category="snf-branding-settings",
     export=True,
+    configure_callback=_auto_configure_branding_image_media_url,
 )
 
 BRANDING_FAVICON_URL = Default(
-    deault_value=BRANDING_IMAGE_MEDIA_URL + "favicon.ico",
-    description="The service's favicon that will appear on the browser's "
-        "address bar.",
-    dependencies=[BRANDING_IMAGE_MEDIA_URL],
+    default_value="favicon.ico",
+    description=(
+        "The service's favicon that will appear on the browser's "
+        "address bar."),
+    dependencies=['BRANDING_IMAGE_MEDIA_URL'],
     category="snf-branding-settings",
     export=True,
+    configure_callback=auto_configure_append_default_value,
 )
 
 BRANDING_DASHBOARD_LOGO_URL = Default(
-    deault_value=BRANDING_IMAGE_MEDIA_URL + "dashboard_logo.png",
-    description="The service logo that appears on all Dashboard (Astakos) "
-        "pages.",
-    dependencies=[BRANDING_IMAGE_MEDIA_URL],
+    default_value="dashboard_logo.png",
+    description=(
+        "The service logo that appears on all Dashboard (Astakos) pages."),
+    dependencies=['BRANDING_IMAGE_MEDIA_URL'],
     category="snf-branding-settings",
     export=True,
+    configure_callback=auto_configure_append_default_value,
 )
 
 BRANDING_COMPUTE_LOGO_URL = Default(
-    deault_value=BRANDING_IMAGE_MEDIA_URL + "compute_logo.png",
-    description="The service logo that appears on all Compute and Network "
-        "(Cyclades) pages.",
-    dependencies=[BRANDING_IMAGE_MEDIA_URL],
+    default_value="compute_logo.png",
+    description=(
+        "The service logo that appears on all Compute and Network "
+        "(Cyclades) pages."),
+    dependencies=['BRANDING_IMAGE_MEDIA_URL'],
     category="snf-branding-settings",
     export=True,
+    configure_callback=auto_configure_append_default_value,
 )
 
 BRANDING_CONSOLE_LOGO_URL = Default(
-    deault_value=BRANDING_IMAGE_MEDIA_URL + "console_logo.png",
-    description="The service logo that appears on the VM Console page "
-        "(in Cyclades).",
-    dependencies=[BRANDING_IMAGE_MEDIA_URL],
+    default_value="console_logo.png",
+    description=(
+        "The service logo that appears on the VM Console page "
+        "(in Cyclades)."),
+    dependencies=['BRANDING_IMAGE_MEDIA_URL'],
     category="snf-branding-settings",
     export=True,
+    configure_callback=auto_configure_append_default_value,
 )
 
 BRANDING_STORAGE_LOGO_URL = Default(
-    deault_value=BRANDING_IMAGE_MEDIA_URL + "storage_logo.png",
-    description="The service logo that appears on all Storage (Pithos) "
-        "pages.",
-    dependencies=[BRANDING_IMAGE_MEDIA_URL],
+    default_value="storage_logo.png",
+    description=(
+        "The service logo that appears on all Storage (Pithos) pages."),
+    dependencies=['BRANDING_IMAGE_MEDIA_URL'],
     category="snf-branding-settings",
     export=True,
+    configure_callback=auto_configure_append_default_value,
 )
 
 #
@@ -105,15 +134,26 @@ BRANDING_SHOW_COPYRIGHT = Default(
     export=True,
 )
 
-copyright_period_default = "2011-%s" % (datetime.datetime.now().year)
-copyright_message_default = \
-    "Copyright (c) %s %s" % (copyright_period_default, BRANDING_COMPANY_NAME)
 
-BRANDING_COPYRIGHT_MESSAGE = Default(
-    default_value=copyright_message_default,
-    description="The copyright message that will appear in the UI's footer. "
-        "Defaults to 'Copyright (c) 2011-<now> <BRANDING_COMPANY_NAME>'",
-    dependencies=[BRANDING_SHOW_COPYRIGHT],
+def _auto_configure_copyright_message(setting, value, deps):
+    if value is not Setting.NoValue:
+        # acknowledge user-provided setting
+        return Setting.NoValue
+
+    # Auto-configure a default one
+    copyright_period_default = "2011-%s" % (datetime.datetime.now().year)
+    copyright_message_default = "Copyright (c) %s %s" % (
+        copyright_period_default, deps['BRANDING_COMPANY_NAME'])
+    return copyright_message_default
+
+
+BRANDING_COPYRIGHT_MESSAGE = Auto(
+    default_value=None,
+    description=(
+        "The copyright message that will appear in the UI's footer. "
+        "Defaults to 'Copyright (c) 2011-<now> <BRANDING_COMPANY_NAME>'"),
+    dependencies=['BRANDING_SHOW_COPYRIGHT', 'BRANDING_COMPANY_NAME'],
     category="snf-branding-settings",
     export=True,
+    configure_callback=_auto_configure_copyright_message,
 )
--- snf-branding/synnefo_branding/utils.py
+++ snf-branding/synnefo_branding/utils.py
@@ -14,7 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.conf import settings as django_settings
-from synnefo_branding import settings
+from synnefo_branding import branding_settings
 from django.template.loader import render_to_string as django_render_to_string
 
 
@@ -22,12 +22,12 @@ def get_branding_dict(prepend=None):
     # CONTACT_EMAIL may not be a branding setting. We include it here though
     # for practial reasons.
     dct = {'support': django_settings.CONTACT_EMAIL}
-    for key in dir(settings):
+    for key in dir(branding_settings):
         if key == key.upper():
             newkey = key.lower()
             if prepend:
                 newkey = '%s_%s' % (prepend, newkey)
-            dct[newkey.upper()] = getattr(settings, key)
+            dct[newkey.upper()] = getattr(branding_settings, key)
     return dct
 
 
