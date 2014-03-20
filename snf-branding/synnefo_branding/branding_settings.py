0c9996a91b66c2d3f3ff0e19ee20bb1140cf9d62
Giorgos Korfiatis <gkorf@grnet.gr>
2014-03-20 15:49:04 +0200

    fix branding settings

--- snf-branding/synnefo_branding/branding_settings.py
+++ snf-branding/synnefo_branding/branding_settings.py
@@ -12,28 +12,6 @@ DASHBOARD_LOGO_URL = settings.BRANDING_DASHBOARD_LOGO_URL
 COMPUTE_LOGO_URL = settings.BRANDING_COMPUTE_LOGO_URL
 CONSOLE_LOGO_URL = settings.BRANDING_CONSOLE_LOGO_URL
 
-## Images
-######################
-
-# The default path to the folder that contains all branding images
-IMAGE_MEDIA_URL = getattr(settings, 'BRANDING_IMAGE_MEDIA_URL',
-                          settings.MEDIA_URL+'branding/images/')
-
-# The service favicon
-FAVICON_URL = getattr(settings, 'BRANDING_FAVICON_URL',
-                      IMAGE_MEDIA_URL+'favicon.ico')
-# Logo used in Dashboard pages (Astakos)
-DASHBOARD_LOGO_URL = getattr(settings, 'BRANDING_DASHBOARD_LOGO_URL',
-                             IMAGE_MEDIA_URL+'dashboard_logo.png')
-# Logo used in Compute pages (Cyclades)
-COMPUTE_LOGO_URL = getattr(settings, 'BRANDING_COMPUTE_LOGO_URL',
-                           IMAGE_MEDIA_URL+'compute_logo.png')
-# Logo used in Console page for VM (Cyclades)
-CONSOLE_LOGO_URL = getattr(settings, 'BRANDING_CONSOLE_LOGO_URL',
-                           IMAGE_MEDIA_URL+'console_logo.png')
-# Logo used in Storage pages (Pithos)
-STORAGE_LOGO_URL = getattr(settings, 'BRANDING_STORAGE_LOGO_URL',
-                           IMAGE_MEDIA_URL+'storage_logo.png')
 
 ## Copyright and footer options
 ######################
