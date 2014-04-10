ba715a6e3145315bb23f244862b756dd716b5281
Giorgos Korfiatis <gkorf@grnet.gr>
2014-04-10 16:50:39 +0300

    annotate oa2 settings

--- snf-astakos-app/astakos/oa2/settings.py
+++ snf-astakos-app/astakos/oa2/settings.py
@@ -1,27 +1,11 @@
 from synnefo import settings
 
-
-def get_setting(key, default):
-    return getattr(settings, 'OAUTH2_%s' % key, default)
-
-USER_MODEL = get_setting('USER_MODEL', 'auth.User')
-
-ENDPOINT_PREFIX = get_setting('ENDPOINT_PREFIX', 'oauth2/')
-
-TOKEN_ENDPOINT = get_setting('TOKEN_ENDPOINT', 'token/')
-
-AUTHORIZATION_ENDPOINT = get_setting('AUTHORIZATION_ENDPOINT', 'auth/')
-
-# Set the length of newly created authorization codes to 60 characters
-AUTHORIZATION_CODE_LENGTH = get_setting('AUTHORIZATION_CODE_LENGTH', 60)
-
-# Set the length of newly created access tokens to 30 characters
-TOKEN_LENGTH = get_setting('TOKEN_LENGTH', 30)
-
-# Set the expiration time of newly created access tokens to 20 seconds
-TOKEN_EXPIRES = get_setting('TOKEN_EXPIRES', 20)
-
-# Set the maximum allowed redirection endpoint URI length
-# Requests for a greater redirection endpoint URI will fail.
-MAXIMUM_ALLOWED_REDIRECT_URI_LENGTH = get_setting(
-    'MAXIMUM_ALLOWED_REDIRECT_URI_LENGTH', 5000)
+USER_MODEL = settings.OAUTH2_USER_MODEL
+ENDPOINT_PREFIX = settings.OAUTH2_ENDPOINT_PREFIX
+TOKEN_ENDPOINT = settings.OAUTH2_TOKEN_ENDPOINT
+AUTHORIZATION_ENDPOINT = settings.OAUTH2_AUTHORIZATION_ENDPOINT
+AUTHORIZATION_CODE_LENGTH = settings.OAUTH2_AUTHORIZATION_CODE_LENGTH
+TOKEN_LENGTH = settings.OAUTH2_TOKEN_LENGTH
+TOKEN_EXPIRES = settings.OAUTH2_TOKEN_EXPIRES
+MAXIMUM_ALLOWED_REDIRECT_URI_LENGTH = \
+    settings.OAUTH2_MAXIMUM_ALLOWED_REDIRECT_URI_LENGTH
