485ffa21efab3f89360b28a76e28a57ccf8e9fcf
Giorgos Korfiatis <gkorf@grnet.gr>
2014-06-27 14:55:22 +0300

    branding: Update external references

--- snf-cyclades-app/synnefo/cyclades/ui/views.py
+++ snf-cyclades-app/synnefo/cyclades/ui/views.py
@@ -22,7 +22,7 @@ from django.template import loader
 from django.http import HttpResponse
 from django.utils.translation import get_language
 from django.utils import simplejson as json
-from synnefo_branding.utils import render_to_string
+from synnefo.branding.utils import render_to_string
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
 
