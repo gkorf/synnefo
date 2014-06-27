ba10c59bd5d211d726d26121bccf1c8af2c63bad
Giorgos Korfiatis <gkorf@grnet.gr>
2014-06-27 14:43:20 +0300

    branding: Update references

--- snf-branding/synnefo/branding/utils.py
+++ snf-branding/synnefo/branding/utils.py
@@ -14,7 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.conf import settings as django_settings
-from synnefo_branding import settings
+from synnefo.branding import settings
 from django.template.loader import render_to_string as django_render_to_string
 
 
