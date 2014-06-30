4a4f7a6919f72f9bdb043b094b2dbfc638b43c5b
Giorgos Korfiatis <gkorf@grnet.gr>
2014-06-30 14:39:22 +0300

    snf_django: Update references

--- snf-django-lib/synnefo/django/utils/transaction.py
+++ snf-django-lib/synnefo/django/utils/transaction.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from snf_django.utils.db import select_db
+from synnefo.django.utils.db import select_db
 
 
 def _transaction_func(app, method, using):
