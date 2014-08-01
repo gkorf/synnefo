df93da27d7f2a6fece834fc735ea6bbd32054866
Giorgos Korfiatis <gkorf@grnet.gr>
2014-08-01 15:45:28 +0300

    settings infrastructure

--- snf-common/synnefo/settings/__init__.py
+++ snf-common/synnefo/settings/__init__.py
@@ -14,23 +14,31 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
-import sys
-
-from synnefo.util.entry_points import extend_settings
+from sys import modules, stderr
+_module = modules[__name__]
 
 # set synnefo package __file__ to fix django related bug
 import synnefo
 synnefo.__file__ = os.path.join(synnefo.__path__[0], '__init__.py')
 
-# import default settings
-from synnefo.settings.default import *
+from synnefo.util.entry_points import get_entry_points
+from synnefo.lib.settings import setup
 
 # autodetect default settings provided by synnefo applications
-extend_settings(__name__, 'synnefo')
+for e in get_entry_points('synnefo', 'default_settings'):
+    m = e.load()
+    setup.initialize_module(m)
+
+setup.preconfigure_settings()
+
+#_module.__dict__.update(setup.Catalogs['defaults'])
 
 # extend default settings with settings provided within *.conf user files
 # located in directory specified in the SYNNEFO_SETTINGS_DIR
 # environment variable
+import re
+system_conf_re = re.compile('^([0-9]\+-)?system.conf$')
+
 SYNNEFO_SETTINGS_DIR = os.environ.get('SYNNEFO_SETTINGS_DIR', "/etc/synnefo/")
 if os.path.exists(SYNNEFO_SETTINGS_DIR):
     try:
@@ -39,18 +47,35 @@ if os.path.exists(SYNNEFO_SETTINGS_DIR):
         conffiles = [f for f in entries if os.path.isfile(f) and
                      f.endswith(".conf")]
     except Exception as e:
-        print >> sys.stderr, "Failed to list *.conf files under %s" % \
-                             SYNNEFO_SETTINGS_DIR
+        print >> stderr, "Failed to list *.conf files under %s" % \
+            SYNNEFO_SETTINGS_DIR
         raise SystemExit(1)
     conffiles.sort()
     for f in conffiles:
+        if system_conf_re.match(f):
+            allow_known = False
+            allow_unknown = True
+        else:
+            allow_known = True
+            allow_unknown = False
+
+        # FIXME: Hack until all settings have been annotated properly
+        allow_unknown = True
+        allow_override = True
+
         try:
-            execfile(os.path.abspath(f))
+            path = os.path.abspath(f)
+            setup.load_configuration(path,
+                                     allow_known=allow_known,
+                                     allow_unknown=allow_unknown,
+                                     allow_override=allow_override)
         except Exception as e:
-            print >> sys.stderr, "Failed to read settings file: %s [%r]" % \
-                                 (os.path.abspath(f), e)
+            print >> stderr, "Failed to read settings file: %s [%r]" % \
+                (path, e)
             raise SystemExit(1)
 
+setup.configure_settings()
+_module.__dict__.update(setup.Catalogs['runtime'])
 
 from os import environ
 # The tracing code is enabled by an environmental variable and not a synnefo
@@ -59,3 +84,13 @@ from os import environ
 if environ.get('SYNNEFO_TRACE'):
     from synnefo.lib import trace
     trace.set_signal_trap()
+
+# cleanup module namespace
+# WARNING: this may prevent synnefo.settings to have sub-modules,
+#          e.g. synnefo.settings.test
+for _name in dir(_module):
+    if _name.startswith('_') or _name.isupper():
+        continue
+    delattr(_module, _name)
+del _name
+del _module
