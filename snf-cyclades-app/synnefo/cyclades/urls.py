6bab355ef8547f90e6bc64bd37a1caea99608cf8
Giorgos Korfiatis <gkorf@grnet.gr>
2014-08-01 16:22:12 +0300

    cyclades services and urls
    Conflicts:
    	snf-cyclades-app/synnefo/cyclades/api/tests/flavors.py
    	snf-cyclades-app/synnefo/cyclades/api/tests/images.py
    	snf-cyclades-app/synnefo/cyclades/api/tests/networks.py
    	snf-cyclades-app/synnefo/cyclades/api/tests/servers.py
    	snf-cyclades-app/synnefo/cyclades/api/tests/versions.py
    	snf-cyclades-app/synnefo/cyclades/api/util.py
    	snf-cyclades-app/synnefo/cyclades/cyclades_settings.py
    	snf-cyclades-app/synnefo/cyclades/quotas/resources.py
    	snf-cyclades-app/synnefo/cyclades/ui/settings.py
    	snf-cyclades-app/synnefo/cyclades/urls.py

--- snf-cyclades-app/synnefo/cyclades/urls.py
+++ snf-cyclades-app/synnefo/cyclades/urls.py
@@ -16,90 +16,85 @@
 from django.conf.urls import patterns, include
 
 from synnefo.django.lib.api.proxy import proxy
-from synnefo.django.lib.api.utils import prefix_pattern
+from synnefo.django.lib.api.utils import prefix_pattern, prefix_pattern_of
 from synnefo.django.utils.urls import \
     extend_with_root_redirects, extend_endpoint_with_slash
 from synnefo.django.lib.api.urls import api_patterns
-from synnefo.cyclades.cyclades_settings import (
-    BASE_PATH, COMPUTE_PREFIX, NETWORK_PREFIX, VMAPI_PREFIX,
-    PLANKTON_PREFIX, HELPDESK_PREFIX, UI_PREFIX, VOLUME_PREFIX,
-    USERDATA_PREFIX, ADMIN_PREFIX,
-    ASTAKOS_AUTH_PROXY_PATH, ASTAKOS_AUTH_URL,
-    ASTAKOS_ACCOUNT_PROXY_PATH, ASTAKOS_ACCOUNT_URL,
-    ASTAKOS_UI_PROXY_PATH, ASTAKOS_UI_URL,
-    cyclades_services)
+from django.conf import settings
 
 from functools import partial
 
 
 urlpatterns = []
 
+cyclades_services = settings.CYCLADES_SERVICES
 # Redirects should be first, otherwise they may get overridden by wildcards
 extend_endpoint_with_slash(urlpatterns, cyclades_services, 'cyclades_ui')
 extend_endpoint_with_slash(urlpatterns, cyclades_services, 'cyclades_helpdesk')
-extend_endpoint_with_slash(urlpatterns, cyclades_services, 'admin')
+extend_endpoint_with_slash(urlpatterns, cyclades_services, 'cyclades_admin')
 extend_endpoint_with_slash(urlpatterns, cyclades_services, 'cyclades_userdata')
 
 cyclades_patterns = api_patterns(
     '',
-    (prefix_pattern(VMAPI_PREFIX),
+    (prefix_pattern_of('cyclades_vmapi'),
      include('synnefo.cyclades.vmapi.urls')),
-    (prefix_pattern(PLANKTON_PREFIX),
+    (prefix_pattern_of('cyclades_plankton'),
      include('synnefo.cyclades.plankton.urls')),
-    (prefix_pattern(COMPUTE_PREFIX),
+    (prefix_pattern_of('cyclades_compute'),
      include('synnefo.cyclades.api.compute_urls')),
-    (prefix_pattern(NETWORK_PREFIX),
+    (prefix_pattern_of('cyclades_network'),
      include('synnefo.cyclades.api.network_urls')),
-    (prefix_pattern(USERDATA_PREFIX),
+    (prefix_pattern_of('cyclades_userdata'),
      include('synnefo.cyclades.userdata.urls')),
-    (prefix_pattern(ADMIN_PREFIX),
+    (prefix_pattern_of('cyclades_admin'),
      include('synnefo.cyclades.admin.urls')),
-    (prefix_pattern(VOLUME_PREFIX),
+    (prefix_pattern_of('cyclades_volume'),
      include('synnefo.cyclades.volume.urls')),
 )
 
 cyclades_patterns += patterns(
     '',
-    (prefix_pattern(UI_PREFIX), include('synnefo.cyclades.ui.urls')),
+    (prefix_pattern_of('cyclades_ui'), include('synnefo.cyclades.ui.urls')),
 )
 
 cyclades_patterns += api_patterns(
     '',
-    (prefix_pattern(HELPDESK_PREFIX),
+    (prefix_pattern_of('cyclades_helpdesk'),
      include('synnefo.cyclades.helpdesk.urls')),
 )
 
 urlpatterns += patterns(
     '',
-    (prefix_pattern(BASE_PATH), include(cyclades_patterns)),
+    (prefix_pattern(settings.CYCLADES_BASE_PATH), include(cyclades_patterns)),
 )
 
 
 # --------------------------------------
 # PROXY settings
 astakos_auth_proxy = \
-    partial(proxy, proxy_base=ASTAKOS_AUTH_PROXY_PATH,
-            target_base=ASTAKOS_AUTH_URL)
+    partial(proxy, proxy_base=settings.ASTAKOS_AUTH_PROXY_PATH,
+            target_base=settings.ASTAKOS_AUTH_URL)
 astakos_account_proxy = \
-    partial(proxy, proxy_base=ASTAKOS_ACCOUNT_PROXY_PATH,
-            target_base=ASTAKOS_ACCOUNT_URL)
+    partial(proxy, proxy_base=settings.ASTAKOS_ACCOUNT_PROXY_PATH,
+            target_base=settings.ASTAKOS_ACCOUNT_URL)
 
 # ui views serve html content, redirect instead of proxing
 astakos_ui_proxy = \
-    partial(proxy, proxy_base=ASTAKOS_UI_PROXY_PATH,
-            target_base=ASTAKOS_UI_URL, redirect=True)
+    partial(proxy, proxy_base=settings.ASTAKOS_UI_PROXY_PATH,
+            target_base=settings.ASTAKOS_UI_URL, redirect=True)
 
 urlpatterns += api_patterns(
     '',
-    (prefix_pattern(ASTAKOS_AUTH_PROXY_PATH), astakos_auth_proxy),
-    (prefix_pattern(ASTAKOS_ACCOUNT_PROXY_PATH), astakos_account_proxy),
+    (prefix_pattern(settings.ASTAKOS_AUTH_PROXY_PATH), astakos_auth_proxy),
+    (prefix_pattern(settings.ASTAKOS_ACCOUNT_PROXY_PATH),
+     astakos_account_proxy),
 )
 urlpatterns += patterns(
     '',
-    (prefix_pattern(ASTAKOS_UI_PROXY_PATH), astakos_ui_proxy),
+    (prefix_pattern(settings.ASTAKOS_UI_PROXY_PATH), astakos_ui_proxy),
 )
 
 # --------------------------------------
 # set utility redirects
-extend_with_root_redirects(urlpatterns, cyclades_services, 'cyclades_ui',
-                           BASE_PATH)
+extend_with_root_redirects(urlpatterns, cyclades_services,
+                           'cyclades_ui', settings.CYCLADES_BASE_PATH)
