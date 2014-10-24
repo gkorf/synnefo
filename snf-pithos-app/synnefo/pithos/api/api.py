27d176231d1a42752ca8ccb8c8c4e8df890f91f7
Giorgos Korfiatis <gkorf@grnet.gr>
2014-10-24 12:53:53 +0300

    fix api lib more

--- snf-pithos-app/synnefo/pithos/api/api.py
+++ snf-pithos-app/synnefo/pithos/api/api.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from synnefo.cyclades.api import api as api_lib
+from synnefo.django.lib import api as api_lib
 from synnefo.django.lib.api import *
 from django.conf import settings
 
